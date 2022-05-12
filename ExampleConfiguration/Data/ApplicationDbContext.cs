0ecc86052cd31c01a47906bf395ad318695c2ec0
kirilvankov <kirilevankov@gmail.com>
2022-05-12 23:40:50 +0300

    Add project files.

---
+++ ExampleConfiguration/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿namespace ExampleConfiguration.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using ExampleConfiguration.DataModels;
+
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public ApplicationDbContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Setting> Settings { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.\\sqlexpress;Database=SettingDb;TrustedConnection=True");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ ExampleConfiguration/DataModels/Setting.cs
@@ -0,0 +1,14 @@
+﻿namespace ExampleConfiguration.DataModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class Setting
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
